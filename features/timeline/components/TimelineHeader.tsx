872f55384cfbd063995248062687f136e13353b2
awashimakentaro <awashimanman@gmail.com>
2026-03-07 14:08:11 +0900

    refractor(timeline): make header  ui components   timeline/components/TimelineHeader.tsx

---
+++ features/timeline/components/TimelineHeader.tsx
@@ -0,0 +1,8 @@
+export const TimelineHeader = () => {
+  return (
+    <header className="mb-6">
+      <h1 className="font-bold text-gray-700 text-2xl leading-10">タイムライン</h1>
+      <p className="text-sm font-medium leading-5 text-slate-400">大切な仲間の歩み</p>
+    </header>
+  );
+};
