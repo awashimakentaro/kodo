c54844cb1c8c8d20d35ebeb46056251f67df6133
awashimakentaro <awashimanman@gmail.com>
2026-03-07 14:13:45 +0900

    refractor(timeline): make EmptyTimeline ui components this ui show when there is no user  timeline/components/EmptyTimeline.tsx

---
+++ features/timeline/components/EmptyTimeline.tsx
@@ -0,0 +1,7 @@
+export const EmptyTimeline = () => {
+  return (
+    <div className="rounded-2xl border border-slate-100 bg-slate-50 px-4 py-6 text-center text-sm text-slate-500">
+      昨日はフォロワーによる投稿はありません
+    </div>
+  );
+};
