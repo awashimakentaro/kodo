5e0ab9de64f180821a53d786ee5a81def8498b13
awashimakentaro <awashimanman@gmail.com>
2026-03-14 01:25:27 +0900

    refractor(today): move reflection showing logic and refact page.tsx features/today/components/UserPrevPost.tsx

--- app/today/page.tsx
+++ app/today/page.tsx
@@ -1,11 +1,6 @@
 "use client";
-
-import { useEffect, useState } from "react";
 import { useAuth } from "../providers/AuthProvider";
-import { TaskTextList } from "@/features/today/types/type";
 import { getYesterdayYmd } from "@/lib/date";
-import { fetchPostTasks } from "@/features/today/services/taskService";
-import { fetchMyPostReflection } from "@/features/today/services/reflectionService";
 import { usePrevTasks } from "@/features/today/hooks/usePrevTasks";
 import { TodayHeader } from "@/features/today/components/TodayHeader";
 import { UserPrevPost } from "@/features/today/components/UserPrevPost";
@@ -13,39 +8,16 @@ import { UserPrevTasks } from "@/features/today/components/UserPrevTasks";
 
 export default function today() {
   const { user } = useAuth();
-  const [prevTasks, setPrevTasks] = useState<TaskTextList>([]);
-  const [prevPost, setPrevPost] = useState("");
-  const [selectDate, setSelectDate] = useState("");
   const ymd = getYesterdayYmd();
 
   const { tasks, isLoading: tasksLoading } = usePrevTasks(user?.id, ymd);
-
-  useEffect(() => {
-    if (!user?.id) return;
-
-    const targetDate = selectDate || ymd;
-
-    fetchPostTasks(user.id, ymd).then(setPrevTasks);
-    fetchMyPostReflection(user.id, targetDate).then(setPrevPost);
-  }, [user?.id, selectDate]);
-
   return (
     <div className="min-h-screen bg-white">
       <div className="mx-auto w-full max-w-md px-5 pb-40 pt-6 ">
         <div className="px-5 pb-10 pt-6">
           <TodayHeader />
           <UserPrevTasks tasks={tasks} />
-          <UserPrevPost />
-
-          <div className="mt-6 rounded-[32px] border border-slate-100 bg-white px-6 py-8">
-            {prevPost ? (
-              <p className="text-sm leading-6 text-slate-700 whitespace-pre-wrap break-words">
-                {prevPost}
-              </p>
-            ) : (
-              <p className="text-center text-sm text-slate-400">この日の投稿はないよ</p>
-            )}
-          </div>
+          <UserPrevPost userId={user?.id} defaultDate={ymd} />
         </div>
       </div>
     </div>
--- features/today/components/UserPrevPost.tsx
+++ features/today/components/UserPrevPost.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { usePrevPost } from "../hooks/usePrevPost";
 
 const schema = z.object({
   selectDate: z.string().min(1, "日付を選択してください"), //selectDateというフィールド名をここで定義している
@@ -10,7 +11,7 @@ type FormValues = z.infer<typeof schema>; //ここで型を生成し
 //     selectDate: string
 //   }これが生成結果
 
-export const UserPrevPost = () => {
+export const UserPrevPost = ({ userId, defaultDate }: { userId?: string; defaultDate: string }) => {
   const {
     //これはRHFの初期設定
     register, //inputとrhfを紐づける関数これがないとそもそも始まらない
@@ -18,7 +19,7 @@ export const UserPrevPost = () => {
     formState: { errors },
   } = useForm<FormValues>({
     resolver: zodResolver(schema), //schemaのルールや形を受容したregister watch formstateが作られる
-    defaultValues: { selectDate: "" },
+    defaultValues: { selectDate: defaultDate },
   });
   // UseFormProps = {　RHFにわたせるprops
   //     resolver
@@ -31,6 +32,7 @@ export const UserPrevPost = () => {
   //見にくいけど文法としては　const {a, b, c} = useForm() みたいなもん分割代入
 
   const selectDate = watch("selectDate"); // これで値取得
+  const { reflection, isLoading: refrectionLoading } = usePrevPost(userId, selectDate);
 
   return (
     <div className="mt-4">
@@ -40,12 +42,20 @@ export const UserPrevPost = () => {
       <input
         type="date"
         {...register("selectDate")}
-        className="h-11 w-full rounded-xl border border-slate-100 bg-white px-4 text-sm text-slate-700 shadow-sm outline-
-none"
+        className="h-11 w-full rounded-xl border border-slate-100 bg-white px-4 text-sm text-slate-700 shadow-sm outline-none"
       />
       {errors.selectDate && (
         <p className="mt-2 text-xs text-red-500">{errors.selectDate.message}</p>
       )}
+      <div className="mt-6 rounded-[32px] border border-slate-100 bg-white px-6 py-8">
+        {reflection ? (
+          <p className="text-sm leading-6 text-slate-700 whitespace-pre-wrap break-words">
+            {reflection}
+          </p>
+        ) : (
+          <p className="text-center text-sm text-slate-400">この日の投稿はないよ</p>
+        )}
+      </div>
     </div>
   );
 };
